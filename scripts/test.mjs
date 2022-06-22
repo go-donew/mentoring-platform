2bd6ed149ab662c9084abb04c2f00583b5a18b5e
Vedant K <gamemaker0042@gmail.com>
2022-06-22 08:59:25 +0530

    chore: stash

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -2,6 +2,9 @@
 // Watches the `source/` folder for changes and reloads the function then.
 
 import { stdout, env, exit } from 'node:process'
+import { spinner } from 'zx/experimental'
+
+import waitOn from 'wait-on'
 
 import { logger } from './utilities/logger.js'
 import { object } from './utilities/globals.js'
@@ -12,12 +15,36 @@ logger.title('scripts/develop')
 const config = (await fs.readJson('package.json')).env.test
 for (const [variable, value] of object.entries(config)) env[variable] = value
 
-// Then run the tests.
+// Start the emulators.
+let emulators
+await spinner(logger.status('starting emulators'), async () => {
+	// Let the process run in the background, and return when the Emulator UI is up.
+	// eslint-disable-next-line no-unused-expressions
+	emulators = $`pnpm firebase emulators:start --only firestore,auth --project donew-mentoring-api-sandbox`
+	await waitOn({ resources: ['http://localhost:4000'] })
+})
+logger.success('successfully started emulators')
+
+logger.info('running tests')
+stdout.write('\n')
+
 try {
-	await $`pnpm firebase emulators:exec --only firestore,auth --project donew-mentoring-api-sandbox "pnpm jest"`.pipe(
-		stdout,
-	)
+	// Run the tests.
+	await $`pnpm jest`.pipe(stdout)
+
+	// Once they finish, kill the emulators.
+	logger.success('sucessfully ran all tests')
+	logger.end()
+	await spinner(logger.status('stopping emulators'), () => emulators.kill())
+
+	// Then exit successfully.
 	exit(0)
 } catch (error) {
+	// If an error occurs, print an error and kill the emulators.
+	logger.error('one or more tests failed')
+	logger.end()
+	await spinner(logger.status('stopping emulators'), () => emulators.kill())
+
+	// Then exit with a non-zero exit code.
 	exit(error.exitCode)
 }
